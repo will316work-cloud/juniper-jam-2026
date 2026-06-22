a4fdb157686076006d507ead138c2a07f58e06a0
ElseIfElse <sapiszabolcsgame@gmail.com>
2026-06-22 13:01:50 +0200

    Added method explanations

--- Assets/Scripts/Task Manager/TaskProp.cs
+++ Assets/Scripts/Task Manager/TaskProp.cs
@@ -5,13 +5,24 @@ public abstract class TaskProp : MonoBehaviour, IPointerDownHandler, IPointerUpH
 {
     protected GameContext _ctx;
 
+    /// <summary>
+    /// Called when player interacted with the trigger object.
+    /// Should reset the PlayerTask logic to default.
+    /// </summary>
     public abstract void OnTaskStart();
     public virtual void Initialize(GameContext ctx)
     {
         _ctx = ctx;
     }
 
+    /// <summary>
+    /// Called when the left mouse button is pressed down.
+    /// </summary>
     public abstract void PointerDownHandler();
+
+    /// <summary>
+    /// Called when the left mouse button is released.
+    /// </summary>
     public abstract void PointerUpHandler();
 
     public void OnPointerDown(PointerEventData eventData)
