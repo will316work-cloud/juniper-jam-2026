74257c49990023054eef6c6b37cbd1031525a919
g4mer9 <rileyfinkgamer9@gmail.com>
2026-06-22 12:29:34 -0600

    quota, asset credits

--- Assets/Scripts/Interaction system/BatteryDropoff.cs
+++ Assets/Scripts/Interaction system/BatteryDropoff.cs
@@ -15,6 +15,7 @@ public class BatteryDropoff : InteractableObjectInstance
             _ctx.Battery.SwapBattery();
             _ctx.MoneyController.GainMoney(_ctx.Battery.moneyPerDropoff);
             _ctx.WorldHealthMeter.GainHealth(_ctx.Battery.healthPerDropoff);
+            _ctx.Quota.DropOff();
         }
     }
 }
