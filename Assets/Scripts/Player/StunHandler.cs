e51091d8cd6a2b0118e5d86e7e8f57918805797b
ElseIfElse <sapiszabolcsgame@gmail.com>
2026-06-23 14:20:04 +0200

    Minor fixes, tool extension and restart game functionality

--- Assets/Scripts/Player/StunHandler.cs
+++ Assets/Scripts/Player/StunHandler.cs
@@ -11,6 +11,8 @@ public class StunHandler : MonoBehaviour
 
     PlayerControl _playerControl;
 
+    public bool IsDebugOn;
+
     public void Initialize(PlayerControl playerControl)
     {
         _playerControl = playerControl;
@@ -18,12 +20,12 @@ public class StunHandler : MonoBehaviour
 
     IEnumerator StunRoutine()
     {
-        Debug.Log("Player is stunned");
+        if(IsDebugOn) Debug.Log("Player is stunned");
         _playerControl.AddMovementBlockReason(MovementBlockReason.Stun);
         _isStunned = true;
 
         yield return new WaitForSeconds(StunDuration);
-        Debug.Log("Player is not stunned anymore. Player is stun protected");
+        if(IsDebugOn) Debug.Log("Player is not stunned anymore. Player is stun protected");
         _isStunned = false;
         _isProtectedFromStun = true;
         _playerControl.RemoveMovementBlockReason(MovementBlockReason.Stun);
@@ -34,7 +36,7 @@ public class StunHandler : MonoBehaviour
     {
         yield return new WaitForSeconds(StunProtectionDuration);
         _isProtectedFromStun = false;
-        Debug.Log("Stun protection is over");
+        if(IsDebugOn) Debug.Log("Stun protection is over");
     }
 
     void OnTriggerEnter(Collider other)
