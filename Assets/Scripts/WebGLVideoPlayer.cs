8162db5cc288ca7e8c16f2e623ae4eedee3dbc94
g4mer9 <rileyfinkgamer9@gmail.com>
2026-06-26 18:15:43 -0600

    WebGL HowToFix, 16:9 fix

--- Assets/Scripts/WebGLVideoPlayer.cs
+++ Assets/Scripts/WebGLVideoPlayer.cs
@@ -4,11 +4,12 @@ using UnityEngine.Video;
 public class WebGLVideoPlayer : MonoBehaviour
 {
     private VideoPlayer videoPlayer;
+    [SerializeField] private string _videoFileName;
 
     void Start()
     {
         videoPlayer = GetComponent<VideoPlayer>();
-        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, "Spin.mp4");
+        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, _videoFileName);
         videoPlayer.url = videoPath;
         videoPlayer.Prepare();
     }
